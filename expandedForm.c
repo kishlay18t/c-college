13c87963774c2573685860e3644a5417f324a75e
Kishu <kishlay18t@gmail.com>
2025-09-16 11:10:01 +0530

    Realised that extra nested for loop was useless

--- expandedForm.c
+++ expandedForm.c
@@ -3,10 +3,12 @@
 #include <stdio.h>
 int main()
 {
-    int n, i, count = 0, place, dig;  // Defining integers being used.
+    int n, place, dig;  // Defining integers being used.
     
     printf ("Enter the number: \n");  // number input
     scanf("%d", &n);
+
+    place = 1;
 
     while (n > 0)  // main while loop. 
     {       
@@ -14,14 +16,7 @@ int main()
 
         n = n/10; // Removing the digit we have taken out already. Count will be 2 next, so second place digit.
 
-        place = 1; // To be multiplied with 10 'count - 1' times
-
-        count++; // Counter for the place of digits. Starts with 1...
-
-        for (i = 1; i < count; i++)  // Loop for number of 0's = place of 'digit -1'
-        {
-            place = place*10;
-        }
+        place = place*10; // To be multiplied with 10 for each place
 
         printf("%d X %d + ", dig, place);  // final printing
     }

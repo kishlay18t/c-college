8a909280fcc6ae691d7e45067195e0b85acfbcf9
Kishu <kishlay18t@gmail.com>
2025-10-30 22:16:26 +0530

    Malloc and Realloc. Dynamic memory allocation.

---
+++ memory/malloc.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    int *p = malloc(5*sizeof(int));
+    for (int i = 0; i < 5; i++)
+    {
+        scanf(" %d", (p+i));
+    }
+
+    for (int i = 0; i < 5; i++)
+    {
+        printf("%d ", *(p+i));
+    }
+
+    return 0;
+}
